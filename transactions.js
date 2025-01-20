d837fd7e5b7f601b4d9615871fb386befc1a4c5d
hidaya052 <hidayaumutoni052@gmail.com>
2025-01-19 17:32:57 -0700

    Add files via upload

---
+++ transactions.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const Transaction = require('../models/Transaction');
+
+// Add a new transaction
+router.post('/', async (req, res) => {
+    const transaction = new Transaction(req.body);
+    try {
+        await transaction.save();
+        res.status(201).send(transaction);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+// Get transactions
+router.get('/', async (req, res) => {
+    try {
+        const transactions = await Transaction.find();
+        res.status(200).send(transactions);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+module.exports = router;
+
